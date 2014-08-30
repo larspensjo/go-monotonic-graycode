7edd4fb84fe8a08d55fb8a613dd2a3ba91b40066
Lars Pensjo <lars.pensjo@gmail.com>
2014-08-30 07:39:13 +0200

    Provide conversion functions

--- MonotonicGrayCode.go
+++ MonotonicGrayCode.go
@@ -13,8 +13,8 @@
 // along with Ephenation.  If not, see <http://www.gnu.org/licenses/>.
 //
 
-// Implementation based on python script http://sciyoshi.com/2010/12/gray-codes/
-
+// Create a sequence of Monotonic Gray Codes.
+// Implementation is based on python script http://sciyoshi.com/2010/12/gray-codes/.
 package monotonic_gray_code
 
 func rotate_right(x []int, n int) []int {
--- exported.go
+++ exported.go
@@ -17,17 +17,29 @@ package monotonic_gray_code
 
 import ()
 
+type Mgc int
+
 var convert_to_int []int
-var convert_to_mgc []int
+var convert_to_mgc []Mgc
 
 // Initialize a sequence of numbers, of length 'count'
 // The tables are pre computed to speed up conversion
 func Init(count int) {
 	convert_to_int = make([]int, count)
-	convert_to_mgc = make([]int, count)
+	convert_to_mgc = make([]Mgc, count)
 	m := monotonic(count)
 	for i, mgc := range m {
 		convert_to_int[getValue(mgc)] = i
-		convert_to_mgc[i] = getValue(mgc)
+		convert_to_mgc[i] = Mgc(getValue(mgc))
 	}
+}
+
+// Convert a Monotonic Gray Code to the corresponding integer. Init() has to called first.
+func GetInt(n Mgc) int {
+	return convert_to_int[n]
+}
+
+// Get the n:th Monotonic Gray Code. Init() has to called first.
+func GetMgc(n int) Mgc {
+	return convert_to_mgc[n]
 }
